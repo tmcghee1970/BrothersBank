8060684c0e485dd434b081d8b3df8e4e5422556f
Tyrone McGhee <tmcghee1970@gmail.com>
2019-11-10 18:53:13 -0500

    Add project files.

---
+++ BrothersBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrothersBank.Startup))]
+namespace BrothersBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
